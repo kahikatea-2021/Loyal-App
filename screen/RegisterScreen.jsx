f521c5d67cced9f58e828851fd9de767e8d59d29
Rogan-Templer <rmtempler@gmail.com>
2021-05-07 11:06:09 +1200

    login page styled

--- screen/RegisterScreen.jsx
+++ screen/RegisterScreen.jsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import {
-    SafeAreaView, TextInput, Text, Button, StyleSheet, KeyboardAvoidingView,
+	SafeAreaView, TextInput, Text, Button, StyleSheet, KeyboardAvoidingView,
 } from 'react-native'
 
 function RegisterScreen ({ navigation }) {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    return (
-        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-            <Text>Hello</Text>
-        </KeyboardAvoidingView>
-    )
+	const [firstName, setFirstName] = useState('')
+	const [lastName, setLastName] = useState('')
+	return (
+		<KeyboardAvoidingView behavior="padding" style={styles.container}>
+			<Text>Hello</Text>
+		</KeyboardAvoidingView>
+	)
 }
 
 const styles = StyleSheet.create({
-    container: {
+	container: {
 
-    },
+	},
 })
 
 export default RegisterScreen
