2cd97c933478562e7115ba85291f739b8a370bbe
Alexander McKerrow <mckerrow4@gmail.com>
2021-05-07 14:00:12 +1200

    authalex

--- screen/HomeScreen.jsx
+++ screen/HomeScreen.jsx
@@ -2,9 +2,18 @@ import React, { useLayoutEffect } from 'react'
 import {
 	SafeAreaView, Button, Text, TouchableOpacity, View,
 } from 'react-native'
+import firebase from 'firebase/app'
 import { auth } from '../auth'
+import 'firebase/firestore'
 
 function HomeScreen ({ navigation }) {
+	firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then((idToken) => {
+		console.log(idToken)
+		// Send token to your backend via HTTPS
+		// ...
+	}).catch((error) => {
+		// Handle error
+	})
 	const signOutUser = () => {
 		auth.signOut().then(() => {
 			navigation.replace('Login')
