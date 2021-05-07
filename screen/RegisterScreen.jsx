842649cb2f1740e219e98caffa7b9c0e19ab3184
Rogan-Templer <rmtempler@gmail.com>
2021-05-07 15:40:01 +1200

    login and register screen now consistent, react-navigation/drawer installed

--- screen/RegisterScreen.jsx
+++ screen/RegisterScreen.jsx
@@ -2,7 +2,7 @@ import { registerRootComponent } from 'expo'
 import React, { useState, useLayoutEffect } from 'react'
 import {
 
-	SafeAreaView, TextInput, Text, Button, StyleSheet, KeyboardAvoidingView, Image,
+	SafeAreaView, TextInput, Text, Button, StyleSheet, KeyboardAvoidingView, Image, ScrollView,
 
 } from 'react-native'
 import { auth } from '../auth/index'
@@ -69,67 +69,70 @@ function RegisterScreen ({ navigation }) {
 	}
 
 	return (
-		<KeyboardAvoidingView behavior="padding" style={styles.container}>
-			<SafeAreaView>
-				<Text style={styles.text}>
-					Create a Loyal Account
+		<KeyboardAvoidingView behavior="position" style={styles.container}>
+			<ScrollView>
+				<SafeAreaView>
+					<Text style={styles.text}>
+						Create a Loyal Account
 
-				</Text>
-				<Image
-					style={styles.logo}
-					source={require('../assets/coffee.jpg')}
-				/>
-				<TextInput
-					placeholder="User Name"
-					autofocus
-					type="text"
-					value={userName}
-					onChangeText={(text) => setUserName(text)}
-				/>
+					</Text>
+					<Image
+						style={styles.logo}
+						source={require('../assets/coffee.jpg')}
+					/>
+					<TextInput
+						style={styles.inputContainer}
+						placeholder="User Name"
+						autofocus
+						type="text"
+						value={userName}
+						onChangeText={(text) => setUserName(text)}
+					/>
 
-				<TextInput
-					style={styles.inputContainer}
-					placeholder="First Name"
-					autofocus
-					type="text"
-					value={firstName}
-					onChangeText={(text) => setFirstName(text)}
-				/>
-				<TextInput
-					style={styles.inputContainer}
-					placeholder="Last Name"
-					type="text"
-					value={lastName}
-					onChangeText={(text) => setLastName(text)}
-				/>
-				<TextInput
-					style={styles.inputContainer}
-					placeholder="Phone Number"
-					type="tel"
-					value={phone}
-					keyboardType="numeric"
-					onChangeText={(num) => setPhone(num)}
-				/>
-				<TextInput
-					style={styles.inputContainer}
-					placeholder="E-mail"
-					type="email"
-					value={email}
-					onChangeText={(text) => setEmail(text)}
-				/>
-				<TextInput
-					style={styles.inputContainer}
-					placeholder="Password"
-					type="password"
-					secureTextEntry
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-					onSubmitEditing={register}
-				/>
-			</SafeAreaView>
-			<SafeAreaView>
-				<Button raised style={styles.button} onPress={register} title="Register" />
-			</SafeAreaView>
+					<TextInput
+						style={styles.inputContainer}
+						placeholder="First Name"
+						autofocus
+						type="text"
+						value={firstName}
+						onChangeText={(text) => setFirstName(text)}
+					/>
+					<TextInput
+						style={styles.inputContainer}
+						placeholder="Last Name"
+						type="text"
+						value={lastName}
+						onChangeText={(text) => setLastName(text)}
+					/>
+					<TextInput
+						style={styles.inputContainer}
+						placeholder="Phone Number"
+						type="tel"
+						value={phone}
+						keyboardType="numeric"
+						onChangeText={(num) => setPhone(num)}
+					/>
+					<TextInput
+						style={styles.inputContainer}
+						placeholder="E-mail"
+						type="email"
+						value={email}
+						onChangeText={(text) => setEmail(text)}
+					/>
+					<TextInput
+						style={styles.inputContainer}
+						placeholder="Password"
+						type="password"
+						secureTextEntry
+						value={password}
+						onChangeText={(text) => setPassword(text)}
+						onSubmitEditing={register}
+					/>
+				</SafeAreaView>
+				<SafeAreaView>
+					<Button raised style={styles.button} onPress={register} title="Register" />
+				</SafeAreaView>
+			</ScrollView>
 		</KeyboardAvoidingView>
 	)
 }
