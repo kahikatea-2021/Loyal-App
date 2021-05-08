c7771cfcd1df43bcc39231a2e06e20886c59f2a1
Sam Thorn <spthornnz@gmail.com>
2021-05-08 14:54:29 +1200

    Makes status bar visible

--- App.jsx
+++ App.jsx
@@ -22,6 +22,7 @@ export default function App () {
 	return (
 		<Provider store={store}>
 			<SafeAreaProvider>
+				<StatusBar />
 				<NavigationContainer>
 					<AppStack.Navigator
 						screenOptions={{
