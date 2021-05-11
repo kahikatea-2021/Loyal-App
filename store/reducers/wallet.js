d11f8408e9b070e3c1f20b358d6d8e77a56ffeeb
Sam Thorn <spthornnz@gmail.com>
2021-05-11 15:52:31 +1200

    Wallet styled with functionality

--- store/reducers/wallet.js
+++ store/reducers/wallet.js
@@ -1,8 +1,10 @@
-import { GET_USER_WALLET } from '../actions/walletAction'
+import { GET_USER_WALLET, DELETE_CARD_FROM_WALLET } from '../actions/walletAction'
 
 function walletReducer(state = [], action) {
 	switch (action.type) {
 	case GET_USER_WALLET:
+		return action.wallet
+	case DELETE_CARD_FROM_WALLET:
 		return action.wallet
 	default:
 		return state
